9efdfaf0806b8df62f14bd125a6e81c28aae9840
Ed Chin <edgardochin@gmail.com>
2022-04-28 22:28:24 -0500

    refactor myResales

--- src/frontend/components/MyResales.js
+++ src/frontend/components/MyResales.js
@@ -1,18 +1,21 @@
 import { useState,
         useEffect,
            useRef } from "react";
+import { ethers } from "ethers";
 import Identicon from 'identicon.js'
-import { ButtonGroup, Button, Card } from "react-bootstrap";
+import { Button, Card, Row, Col } from "react-bootstrap";
 
-/* webpage component  (pass in smart contract & wallet account) */
-const MyResales = ({ contract, account }) => {
+/* webpage component  (pass in smart contract) */
+export default function MyResales ({ contract,account }) {
     //  __ stateful vars __
-    const audioFileRef = useRef(null)
+    const audioFileRef = useRef([])
     const [isPlaying, setIsPlaying] = useState(null)
-    const [currentItemIndex, setCurrentItemIndex] = useState(0)
-    const [itemList, setitemList] = useState(null)
+    const [myTokens, setMyTokens] = useState(null)
+    const [soldItems, setSoldItems] = useState([])
     const [loading, setLoading] = useState(true)
-    const loaditemList = async () => {
+    const [selected, setSelected] = useState(0)
+    const [previous, setPrevious] = useState(null)
+    const loadMyTokens = async () => {
         // get relisted items for account from blockchain
         let filter = contract.filters.MarketItemRelisted(null, account, null)
         let results = await contract.queryFilter(filter)
@@ -40,47 +43,31 @@ const MyResales = ({ contract, account }) => {
             }
             return (item_List)
         }))
-        setitemList(resaleList)  // init the item list
+        setMyTokens(resaleList)
+        // Fetch sold resale items : query MarketItemBought events w/seller = user
+        filter = contract.filters.MarketItemBought(null, account, null, null)
+        results = await contract.queryFilter(filter)
+        // filter out sold items from listed items
+        const filteredItems = resaleList.filter( i => results.some(j => i.itemId.toString() === j.args.tokenId.toString() ))
+        setSoldItems(filteredItems)  //%EDC%
         setLoading(false)
     }
 
-    /* func: skipSong  
-    (true) to go forwards 
-    (false) to go backwards */
-    const skipSong = (forwards) => {
-        if (forwards) {
-            setCurrentItemIndex( () => {
-                let index = currentItemIndex
-                index++
-                if(index > itemList.length - 1) {
-                    index =0}
-                return index
-            })
-        } else {
-            setCurrentItemIndex( () => {
-                let index = currentItemIndex
-                index--
-                if(index < 0) {
-                    index = itemList.length - 1}
-                return index
-            })
-        }
-    } 
-
-    /* ___ useEffects hooks___  
+    /* ___ effects ___  
     useEffect(callback) executes when this component updates 
     (each time  the stateful vars change or component melts) */ 
     useEffect ( () => {
         if (isPlaying) {
-            audioFileRef.current.play()
-        } else if (isPlaying != null) {
-            audioFileRef.current.pause()
+            audioFileRef.current[selected].play()
+            if (selected !== previous) audioFileRef.current[previous].pause()
+        } else if (isPlaying !== null) {
+            audioFileRef.current[selected].pause()
         }
     })
     useEffect( () => {
-        !itemList && loaditemList()   // only load when component mounts (when itemList=null)
+        !myTokens && loadMyTokens()   // only load when component mounts (if value is null)
     })
-
+    
     /* ___ Return HTML ___    */
     if (loading) return (
         // ___ loading message ___
@@ -90,46 +77,70 @@ const MyResales = ({ contract, account }) => {
     );
     return (
         // ___ page elements ___
-        <div className="container-fluid mt-5">
-            <h2>My Listings</h2>
-            {itemList.length > 0 ?
-                <div className="row">
-                    <main role="main" className="col-lg-12 mx-auto" style={{ maxwidth: '500px'}} >
-                        <div className="content mx-auto">
-                            <audio src={itemList[currentItemIndex].audio} ref={audioFileRef} ></audio>
-                            <Card style={{ maxWidth: '30rem' }}>
-                                <Card.Header> {currentItemIndex + 1} of {itemList.length} </Card.Header>
-                                <Card.Img variant="top" src={itemList[currentItemIndex].identicon}/>
+        <div className="flex justify-center">
+            <h1>My Listings</h1>
+            {myTokens.length > 0 ?
+                <div className="px-5 container">
+                    <Row xs={1} md={2} lg={4} className="g-4 py-5">
+                    {myTokens.map((item, idx) => ( 
+                        <Col key={idx} className='overflow-hidden'>
+                            <audio src={item.audio} key={idx} ref={el => audioFileRef.current[idx] =el}></audio>
+                            <Card>
+                                <Card.Img variant="top" src={item.identicon}/>
                                 <Card.Body color="secondary">
-                                    <Card.Title as="h2"> {itemList[currentItemIndex].name}  </Card.Title>
+                                    <Card.Title as="h2">{item.name}</Card.Title>
                                     <div className="d-grid px-4" >
-                                        <ButtonGroup>
-                                            <Button variant="secondary" onClick={() => skipSong(false)} >
-                                                <img src="icons8-prev-48.png" alt='prev'></img>    
-                                            </Button>
-                                            <Button variant="secondary" onClick={() => setIsPlaying(!isPlaying)}>
-                                                {isPlaying ? (
-                                                    <img src="icons8-pause-48.png" alt='pause'></img>    
-                                                ) : (
-                                                    <img src="icons8-play-48.png" alt='play'></img>    
-                                                )}
-                                            </Button>
-                                            <Button variant="secondary" onClick={() => skipSong(true)} >
-                                                <img src="icons8-next-48.png" alt="next" ></img>    
-                                            </Button>
-                                        </ButtonGroup>
+                                        <Button variant="secondary" onClick={() => {
+                                            setPrevious(selected)
+                                            setSelected(idx)
+                                            if (!isPlaying || idx === selected) setIsPlaying(!isPlaying)
+                                        }}>
+                                            { isPlaying && selected === idx ? (    
+                                                <img src="icons8-pause-48.png" alt='pause'></img>    
+                                                            ) : (
+                                                <img src="icons8-play-48.png" alt='play'></img>    
+                                        )}
+                                        </Button>
                                     </div>
+                                    <Card.Text className="mt-1">
+                                        {ethers.utils.formatEther(item.price)} ETH
+                                    </Card.Text>
                                 </Card.Body>
                             </Card>
-                        </div>
-                    </main>
+                        </Col>
+                    ))}
+                    </Row>
+                    <>
+                      <h1>Sold</h1>
+                      {soldItems.length > 0 ?
+                        <Row xs={1} md={2} lg={4} className="g-4 py-5">
+                        {soldItems.map((item, idx) => ( 
+                            <Col key={idx} className='overflow-hidden'>
+                                <Card>
+                                    <Card.Img variant="top" src={item.identicon}/>
+                                    <Card.Body color="secondary">
+                                        <Card.Title as="h2">{item.name}</Card.Title>
+                                        <Card.Text className="mt-1">
+                                            {ethers.utils.formatEther(item.price)} ETH
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                        </Row>
+                        : (
+                          <main style={{ padding: "1rem 0"}}>
+                            <h2>No sold songs</h2>
+                          </main>
+                          )
+                      }
+                    </>
                 </div>
             : (
                 <main style={{ padding: "1rem 0"}}>
-                    <h2>You don't have any songs listed for sale. </h2>
+                    <h2>You don't have any songs for sale.</h2>
                 </main>
             )}
         </div>
     );
 }
-export default MyResales

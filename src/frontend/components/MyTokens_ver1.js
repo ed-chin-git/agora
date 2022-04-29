b68054c622acc5afebbc3fc4d4cf9adb668b1f0b
Ed Chin <edgardochin@gmail.com>
2022-04-28 21:16:31 -0500

    change to multiple cards per page

--- src/frontend/components/MyTokens_ver1.js
+++ src/frontend/components/MyTokens_ver1.js
@@ -3,20 +3,16 @@ import { useState,
            useRef } from "react";
 import { ethers } from "ethers";
 import Identicon from 'identicon.js'
-import { Button, Card, Form, InputGroup, Row, Col } from "react-bootstrap";
+import { ButtonGroup, Button, Card, Form, InputGroup } from "react-bootstrap";
 
 /* webpage component  (pass in smart contract) */
 const MyTokens = ({ contract }) => {
     //  __ stateful vars __
-    const audioFileRef = useRef([])
+    const audioFileRef = useRef(null)
     const [isPlaying, setIsPlaying] = useState(null)
-    // const [currentItemIndex, setCurrentItemIndex] = useState(0)
+    const [currentItemIndex, setCurrentItemIndex] = useState(0)
     const [myTokens, setMyTokens] = useState(null)
     const [loading, setLoading] = useState(true)
-
-    const [selected, setSelected] = useState(0)
-    const [previous, setPrevious] = useState(null)
-
     const [resaleId, setResaleId] = useState(null)
     const [resalePrice, setResalePrice] = useState(null)
     const loadMyTokens = async () => {
@@ -60,15 +56,37 @@ const MyTokens = ({ contract }) => {
       loadMyTokens()
     }
 
+    /* func: skipSong  
+    (true) to go forwards 
+    (false) to go backwards */
+    const skipSong = (forwards) => {
+        if (forwards) {
+            setCurrentItemIndex( () => {
+                let index = currentItemIndex
+                index++
+                if(index > myTokens.length - 1) {
+                    index =0}
+                return index
+            })
+        } else {
+            setCurrentItemIndex( () => {
+                let index = currentItemIndex
+                index--
+                if(index < 0) {
+                    index = myTokens.length - 1}
+                return index
+            })
+        }
+    } 
+
     /* ___ effects ___  
     useEffect(callback) executes when this component updates 
     (each time  the stateful vars change or component melts) */ 
     useEffect ( () => {
         if (isPlaying) {
-            audioFileRef.current[selected].play()
-            if (selected !== previous) audioFileRef.current[previous].pause()
-        } else if (isPlaying !== null) {
-            audioFileRef.current[selected].pause()
+            audioFileRef.current.play()
+        } else if (isPlaying != null) {
+            audioFileRef.current.pause()
         }
     })
     useEffect( () => {
@@ -84,56 +102,56 @@ const MyTokens = ({ contract }) => {
     );
     return (
         // ___ page elements ___
-        <div className="flex justify-center">
+        <div className="container-fluid mt-5">
             <h2>My Library</h2>
             {myTokens.length > 0 ?
-                <div className="px-5 container">
-                    <Row xs={1} md={2} lg={4} className="g-4 py-5">
-                    {myTokens.map((item, idx) => ( 
-                        <Col key={idx} className='overflow-hidden'>
-                            <audio src={item.audio} key={idx} ref={el => audioFileRef.current[idx] =el}></audio>
-                            <Card>
-                                <Card.Img variant="top" src={item.identicon}/>
+                <div className="row">
+                    <main role="main" className="col-lg-12 mx-auto" style={{ maxwidth: '500px'}} >
+                        <div className="content mx-auto">
+                            <audio src={myTokens[currentItemIndex].audio} ref={audioFileRef} ></audio>
+                            <Card style={{ maxWidth: '30rem' }}>
+                                <Card.Header> {currentItemIndex + 1} of {myTokens.length} </Card.Header>
+                                <Card.Img variant="top" src={myTokens[currentItemIndex].identicon}/>
                                 <Card.Body color="secondary">
-                                    <Card.Title as="h2">{item.name}</Card.Title>
+                                    <Card.Title as="h2"> {myTokens[currentItemIndex].name}  </Card.Title>
                                     <div className="d-grid px-4" >
-                                        <Button variant="secondary" onClick={() => {
-                                            setPrevious(selected)
-                                            setSelected(idx)
-                                            if (!isPlaying || idx === selected) setIsPlaying(!isPlaying)
-                                        }}>
-                                            { isPlaying && selected === idx ? (    
-                                                <img src="icons8-pause-48.png" alt='pause'></img>    
-                                                            ) : (
-                                                <img src="icons8-play-48.png" alt='play'></img>    
-                                        )}
-                                        </Button>
+                                        <ButtonGroup>
+                                            <Button variant="secondary" onClick={() => skipSong(false)} >
+                                                <img src="icons8-prev-48.png" alt='prev'></img>    
+                                            </Button>
+                                            <Button variant="secondary" onClick={() => setIsPlaying(!isPlaying)}>
+                                                {isPlaying ? (
+                                                    <img src="icons8-pause-48.png" alt='pause'></img>    
+                                                ) : (
+                                                    <img src="icons8-play-48.png" alt='play'></img>    
+                                                )}
+                                            </Button>
+                                            <Button variant="secondary" onClick={() => skipSong(true)} >
+                                                <img src="icons8-next-48.png" alt="next" ></img>    
+                                            </Button>
+                                        </ButtonGroup>
                                     </div>
-                                    <Card.Text className="mt-1">
-                                        {ethers.utils.formatEther(item.price)} ETH
-                                    </Card.Text>
                                 </Card.Body>
                                 <Card.Footer>
-                                    <InputGroup className="my-1">
-                                        <Button onClick={() => resellItem(item)} variant='outline-primary' id='button-addon1'>
-                                        Resell 
-                                        </Button>
-                                        <Form.Control
+                                  <InputGroup className="my-1">
+                                      <Button onClick={() => resellItem(myTokens[currentItemIndex])} variant='outline-primary' id='button-addon1'>
+                                        Resell
+                                      </Button>
+                                      <Form.Control
                                         onChange={(e) => {
-                                            setResaleId(item.itemId)
-                                            setResalePrice(e.target.value)
+                                          setResaleId(myTokens[currentItemIndex].itemId)
+                                          setResalePrice(e.target.value)
                                         }} 
                                         size='md'
-                                        value={resaleId === item.itemId  ? resalePrice : ''}
+                                        value={resaleId === myTokens[currentItemIndex].itemId  ? resalePrice : ''}
                                         required type="number"
                                         placeholder="Price in ETH"
-                                        />
-                                    </InputGroup>
+                                      />
+                                  </InputGroup>
                                 </Card.Footer>
                             </Card>
-                        </Col>
-                    ))}
-                    </Row>
+                        </div>
+                    </main>
                 </div>
             : (
                 <main style={{ padding: "1rem 0"}}>
